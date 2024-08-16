e39ebd18093f22ae6c1ad74527519d1dbb388813
MrForkle <Aaron_Makes_Games@proton.me>
2024-08-16 18:57:33 -0400

    asas

--- src/UI/EnergyBar/Label.gd
+++ src/UI/EnergyBar/Label.gd
@@ -3,6 +3,5 @@ var energy = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	energy += 0.1
 	text = str(energy)
 	$ProgressBar.value = energy
